82f03ea213b44355f120841bd0199f5e05611266
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-11 04:52:09 -0800

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print('{:s}'.format(chr(ord(c)-32)), end='')
+    print('\n', end='')
