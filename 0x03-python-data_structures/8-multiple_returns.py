0face0ae4f190af6049c77398a7b7b04f128a153
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-13 20:10:26 -0700

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
-    tup = length, first
-    return tup
+    if length == 0:
+        return tup1 = (length, None)
+    else:
+        first = sentence[0]
+        tup = (length, first)
+        return tup
