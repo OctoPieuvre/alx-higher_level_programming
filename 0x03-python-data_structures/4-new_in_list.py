fa6816b0334481392c197d7115f661bc95bad635
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-13 18:54:09 -0700

    Print a list of integers... in reverse!

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_n_list):
+    if idx >= 0 and idx < len(my_list):
         my_new_list = my_list.copy()
         my_new_list[idx] = element
         return my_new_list
     else:
-        return my_list
+        return my_list.copy()
