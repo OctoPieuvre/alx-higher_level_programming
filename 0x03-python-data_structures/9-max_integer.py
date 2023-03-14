a1f6ef557980382d608eb75a5a2fccf5b18d71e5
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-14 06:49:00 -0700

    Find th max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    length =len(my_list)
-    if length = 0:
+    length = len(my_list)
+    if length == 0:
         return None
     else:
         my_list = sorted(my_list)
         maxi = my_list[length-1]
+        return maxi
