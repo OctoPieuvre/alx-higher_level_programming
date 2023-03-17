1c28799389431841431af674a66456439f5c5f67
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-17 12:45:24 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,8 @@ def best_score(a_dictionary):
     a function that return a biggest integer value
     """
 
-    if len(a_dictionary.keys()) == 0:
-        return (None)
+    if not a_dictionary:
+        return None
     else:
         max_key = max(a_dictionary, key=a_dictionary.get)
         return max_key
