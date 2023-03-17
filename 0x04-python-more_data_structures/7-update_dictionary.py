b20a0caed0de7bf844a52b7c249953490f792ffa
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-17 06:51:56 -0700

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return (a_dictionary.update({key: value})) 
+    """
+    replace or add key/value in dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
