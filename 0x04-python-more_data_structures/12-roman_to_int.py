c9ab4091a20b0e505d9ed044d7970193efb9120c
OctoPieuvre <dieudonnezin@outlook.com>
2023-03-18 02:21:56 -0700

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     else:
-        roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         result = 0
         for letter in reversed(roman_string):
             res = roman[letter]
