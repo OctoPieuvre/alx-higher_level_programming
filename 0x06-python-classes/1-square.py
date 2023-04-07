b24cf613119753d83812026d4f0452de38224f98
OctoPieuvre <Dieudonnezin@outlook.com>
2023-04-07 09:22:26 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Define a class Square. """
+
+class Square:
+    """ Represent a Square. """
+    
+    def __init__(self, size):
+        """Initialize a New Square. """
+        self.__size = size
