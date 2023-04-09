063d0a0488509c00a4a7dd2fb6bbbc4a8a489f00
OctoPieuvre <Dieudonnezin@outlook.com>
2023-04-09 13:49:52 -0700

    7. Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Define the Node class"""
+
 class Node:
       """Represent a node in a singly-linked list."""
 
@@ -40,7 +42,37 @@ class Node:
 
 class SinglyLinkedList:
       """Represent a singly-linked list."""
-
       def __init__(self):
           """Initalize a new SinglyLinkedList."""
-          self.head = None
+          self.__head = None
+
+      def __str__(self):
+          """Initialie an empty string to store the output."""
+
+          result = []
+          current = self.__head
+          while current:
+              result += str(current.data) + "\n"
+              current = current.next_node
+
+          return result
+
+      def sorted_insert(self, value):
+          """Create a new node with the given value"""
+
+          new_node = Node(value)
+
+          if self.__head is None:
+              new_node.next_node = None
+              self.__head = new_node
+          elif value < self.__head.data:
+              new_node.next_node = self.__head
+              self.__head = new_node
+          else:
+              current = self.__head
+              previous = None
+              while current and value >= current.data:
+                  previous = current
+                  current = current.next_node
+                  new_node.next_node = current
+                  previous.next_node = new_node
