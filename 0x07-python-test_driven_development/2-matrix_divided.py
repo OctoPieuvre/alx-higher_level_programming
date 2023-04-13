09fa58cac93f74168df7a64ab80660064108069e
OctoPieuvre <Dieudonnezin@outlook.com>
2023-04-13 06:44:14 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -17,8 +17,8 @@ def matrix_divided(matrix, div):
     """
     if (not isinstance(matrix, list) or matrix == [] or not
             all(isinstance(row, list) for row in matrix)
-            or not all((isinstance(el, int) or isinstance(el, float))
-                for el in [num for row in matrix for num in row])):
+                or not all((isinstance(el, int) or isinstance(el, float))
+                    for el in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
                "integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
