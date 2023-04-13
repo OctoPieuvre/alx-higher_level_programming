dee9d2799188fea0df561ee96e4b8887faa6fa00
OctoPieuvre <Dieudonnezin@outlook.com>
2023-04-13 06:37:10 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that divides all elements of a matrix"""
 
+
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix.
     Args:
@@ -14,13 +15,15 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrix representing the result of the division.
     """
-    if (not isinstance(matrix, list) or matrix == [] or not all(isinstance(row, list) for row in matrix) 
-            or not all((isinstance(el, int) or isinstance(el, float)) 
+    if (not isinstance(matrix, list) or matrix == [] or not
+            all(isinstance(row, list) for row in matrix)
+            or not all((isinstance(el, int) or isinstance(el, float))
                 for el in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of "
+               "integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, int) or not isinstance(div, float):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
