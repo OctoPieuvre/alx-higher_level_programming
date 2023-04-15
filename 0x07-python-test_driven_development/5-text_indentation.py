afbe872c64eef43222c81b8e7fcf9d3cef9a5067
OctoPieuvre <Dieudonnezin@outlook.com>
2023-04-15 11:28:50 -0700

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -19,14 +19,14 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    punctuations = [".", "?", ":"]
-    new_text = ""
-    for i in range(len(text)):
-        if text[i] in punctuations:
-            new_text += text[i] + "\n\n"
-            if i < len(text) - 1 and text[i+1] == " ":
+    i = 0
+    while i < len(text):
+        if text[i] in ['.', '?', ':']:
+            print(text[i])
+            print()
+            i += 1
+            while i < len(text) and text[i] == ' ':
                 i += 1
         else:
-            new_text += text[i]
-
-    print("\n".join(line.strip() for line in new_text.split("\n")))
+            print(text[i], end='')
+            i += 1
