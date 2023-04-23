229dd3978aeb9b15dd45c55796417c937a8bb17c
OctoPieuvre <Dieudonnezin@outlook.com>
2023-04-23 01:08:20 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a class that inherit list"""
+
+
+class Mylist(list):
+    """
+    A subclass of list that provides a method to print the list sorted.
+    """
+
+    def print_sorted(self):
+        """
+        Print the list, sorted in ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
