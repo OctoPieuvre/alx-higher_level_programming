0508918e7e11c2bd3fa94b7e0b1b261189498fbf
root <Dieudonnezin@outlook.com>
2023-05-07 00:24:29 -0700

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a file that write in a file"""
+
+
+def write_file(filename="", text=""):
+    """a fonction that write a text in a file.
+
+        Args:
+            filename (str): the name of the file
+            text (str): the thxt to write to file
+        Return:
+            number of character
+    """
+    with open(filename, 'w', encoding='utf8') as file:
+        content = file.write(text)
+        print(content)
