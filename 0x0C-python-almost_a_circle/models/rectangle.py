e4cc044589648a7578eda62270aa469c8a83482a
OctoPieuvre <Dieudonnezin@outlook.com>
2023-05-13 12:55:31 -0700

    Done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-    contains class Rectangle which implements Base.
-"""
+"""contains class Rectangle which implements Base"""
+
 from models.base import Base
 
 
 class Rectangle:
-    """
-        class Rectangle implements Base.
+    """class Rectangle implements Base.
         Methods:
-            __init__()
-    """
+            __init__()"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-            Initializes the instance of the class..
+            Initializes the instance of the class.
         """
         super().__init__(id)
         self.width = width
