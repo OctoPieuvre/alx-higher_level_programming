a51a97a65c88bf630e694357a49392d3e81018ac
OctoPieuvre <Dieudonnezin@outlook.com>
2023-05-14 14:49:43 -0700

    Done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,5 +17,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """overloading string"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
-            self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, 
+            self.x, self.y, self.width)
