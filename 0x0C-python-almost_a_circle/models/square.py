99e6f5f24d4f41f2e4bd3b498909afe34d445167
OctoPieuvre <Dieudonnezin@outlook.com>
2023-05-14 14:28:54 -0700

    Done

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    contains class Square implements class Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        Square implements rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            initialises Square (overrides Rectangle init)
+        """
+        super().__init__(size, size, x, y, id)
+
+
+    def __str__(self):
+        """overloading string"""
+        return "[{}] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+            self.width)
+
